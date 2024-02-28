Made visual 3D rotations and averages. There is however a bug in the averaging, that needs to be sorted.

src/robolie/quaternions/quaternion.py
@@ -160,7 +160,7 @@ class Quaternion:
         else:
             theta = np.arcsin(r)
         return PureQuaternion(*(theta * v))
-    
+
     def which_rotation(self) -> tuple:
         """Returns the corresponding angle and axis of rotation of the unit quaternion."""
         assert np.isclose(self.norm(), 1), "Quaternion must be normalized."
@@ -170,7 +170,7 @@ class Quaternion:
             theta = np.pi - np.arcsin(r)
         else:
             theta = np.arcsin(r)
-        angle = theta/2
+        angle = theta / 2
         return (angle, axis)
 
 
@@ -197,11 +197,11 @@ class PureQuaternion:
     def __mul__(self, other: PureQuaternion) -> PureQuaternion:
         """Multiplies two pure quaternions."""
         return PureQuaternion(*np.cross(self.vector, other.vector))
-    
+
     def __truediv__(self, other: float) -> PureQuaternion:
         """Divides a pure quaternion by a scalar."""
         return PureQuaternion(*(self.vector / other))
-    
+
     def __add__(self, other: PureQuaternion) -> PureQuaternion:
         """Adds two pure quaternions."""
         return PureQuaternion(*(self.vector + other.vector))

src/robolie/quaternions/rotate.py
@@ -4,6 +4,7 @@ Module for 3D rotations using quaternions
 """
 
 from __future__ import annotations
+from typing import Optional
 
 import numpy as np
 
@@ -11,20 +12,61 @@ import robolie as rl
 
 
 def rotate_by_quaternion(
-    vector: np.ndarray, theta: float, axis: np.ndarray
+    vector: np.ndarray,
+    theta: Optional[float] = None,
+    axis: Optional[np.ndarray] = None,
+    quaternion: Optional[rl.Quaternion] = None,
 ) -> np.ndarray:
     """Rotates a vector by a quaternion.
 
     Args:
         vector: The vector to rotate.
-        theta: The angle of rotation in radians.
-        axis: The axis of rotation as a 3D vector.
+        theta Optional[float]: The angle of rotation in radians.
+        axis Optional[ndarray]: The axis of rotation as a 3D vector.
+        quaternion Optional[Quaternion]: The quaternion representing the rotation.
 
     Returns:
         The rotated vector.
     """
-    q: rl.Quaternion = rl.Quaternion.from_angle_and_axis(theta / 2, axis)
     p: rl.Quaternion = rl.Quaternion(0, *vector)
+
+    if quaternion is not None:
+        q = quaternion
+    else:
+        assert np.isclose(np.linalg.norm(axis), 1), "The axis must be a unit vector."
+        q: rl.Quaternion = rl.Quaternion.from_angle_and_axis(theta / 2, axis)
+
     q_conj: rl.Quaternion = q.conjugated()
     p_rot: rl.Quaternion = q * p * q_conj
     return p_rot.vector
+
+
+def compute_average_rotation_quaternion(
+    rotations: list[tuple[float, np.ndarray]]
+) -> rl.Quaternion:
+    """Computes the average of a set of 3D rotations, performed by queternions.
+
+    Args:
+        rotations: A list of rotations, each consisting of an angle and an axis.
+
+    Returns:
+        The average rotation as a Unit Quaternion.
+    """
+
+    # Convert rotations to quaternions
+    quaternions = [
+        rl.Quaternion.from_angle_and_axis(theta / 2, axis) for theta, axis in rotations
+    ]
+
+    # Map all quaternions to the lie algebra
+    pure_quaternions = [rl.log(q) for q in quaternions]
+
+    # Compute the average pure quaternion
+    average_pure_quaternion = rl.PureQuaternion(0, 0, 0)
+    for i in range(len(pure_quaternions)):
+        average_pure_quaternion += pure_quaternions[i]
+    average_pure_quaternion = average_pure_quaternion / len(pure_quaternions)
+
+    # Map the average pure quaternion back to the group using the exponential map
+    average_quaternion = rl.exp(average_pure_quaternion)
+    return average_quaternion
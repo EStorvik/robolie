ce90b49ad6fd7398664dd6710ae7d7b8ae91f05a
EStorvik <erlend.storvik@hvl.no>
2024-02-29 10:38:10 +0100

    Included averaging test

--- tests/test_quaternion.py
+++ tests/test_quaternion.py
@@ -19,3 +19,15 @@ def test_quaternion_multiplication():
     q4 = rl.Quaternion.from_matrix(matrix)
     assert np.allclose(q3.vector, q4.vector)
     assert np.isclose(q3.real, q4.real)
+
+def test_quaternion_averaging():
+    rotations = [
+        (np.pi / 4, np.array([0, 1, 0])),
+        (np.pi / 3, np.array([0, 1/np.sqrt(2), 1/np.sqrt(2)])),
+        (np.pi / 2, np.array([0, 0, 1])),
+    ]
+
+    avg_quaternion = rl.compute_average_rotation_quaternion(rotations)
+    avg_angle, avg_axis = avg_quaternion.which_rotation()
+    assert np.isclose(avg_angle, 0.9553166181245093)
+    assert np.allclose(avg_axis, np.array([0.0, 0.7071067811865475, 0.7071067811865475]))
